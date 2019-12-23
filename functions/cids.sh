0f0850f349e4efea8a4783f04336e13dbc047b98
Casey Flynn <cflynn.us@gmail.com>
2019-12-23 15:50:58 +0800

    Add cids function

---
+++ functions/cids.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# cid.sh
+
+# Container ID Search
+# quickly grab docker container ID for first match of search pattern
+# Ex:
+#   $ docker exec `cids blog` cat /some/file
+#   $ docker diff `cids blog`
+cids() {
+  docker ps | awk "/$1/{print \$1}"
+}
