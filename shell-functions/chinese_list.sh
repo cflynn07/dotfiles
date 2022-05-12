4a12d9770f704c2f8b97fdc0fb91df2a0af76351
Casey Flynn <cflynn.us@gmail.com>
2022-05-12 17:27:00 +0700

    Update chinese_list shell function

---
+++ shell-functions/chinese_list.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# functions/中文單字.sh
+
+# $1 ip address
+# $2 filename
+chinese_list() {
+  # PLECO_FILE="2022-5-w4.xml";
+  HOST=$1;
+  PLECO_FILE=$2;
+  PLECO_TO_ANKI_DIR=/Users/casey/MEGAsync/workspace/plecoToAnki;
+  curl "http://$HOST:50505/$PLECO_FILE.xml" > "$PLECO_TO_ANKI_DIR/$PLECO_FILE.xml";
+  node "$PLECO_TO_ANKI_DIR/index.js" "$PLECO_TO_ANKI_DIR/$PLECO_FILE.xml" > "$HOME/Desktop/$PLECO_FILE.csv";
+  tail -n +2 "$HOME/Desktop/$PLECO_FILE.csv" > "$HOME/Desktop/$PLECO_FILE.csv.tmp"
+  rm "$HOME/Desktop/$PLECO_FILE.csv"
+  mv "$HOME/Desktop/$PLECO_FILE.csv.tmp" "$HOME/Desktop/$PLECO_FILE.csv"
+}
